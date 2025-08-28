09474c1d5f5a3ef4779f0ebba0526a402bba9b3b
kbilly <kemboibilly49@gmail.com>
2025-08-28 20:42:56 +0300

    ADD THE ACCOUNT BALANCE TO $10000 AT ACCOUNT CREATION

--- myapp/models.py
+++ myapp/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
-
+ 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
-    balance = models.DecimalField(max_digits=20, decimal_places=2, default=Decimal("10000.00"))  # $10,000 start
+    balance = models.DecimalField(max_digits=20, decimal_places=2, default=Decimal("20000.00"))  # $20,000 start
 
     def __str__(self):
         return f"{self.user.username} Profile - Balance: {self.balance}"
